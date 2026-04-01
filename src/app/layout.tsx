af1401e42f6d1309c893253bb0b0acbba355024b
Yoga <yogacahayq@gmail.com>
2026-04-01 10:27:59 +0700

    fix: undefined_id_pixel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -99,9 +99,9 @@ export default function RootLayout({
 />   
 
 <noscript>
-  <Image 
-    height={1}
-    width={1}
+  <img 
+    height="1"
+    width="1"
     style={{ display: 'none' }}
     src={`https://www.facebook.com/tr?id=${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}&ev=PageView&noscript=1`}
     alt=""
